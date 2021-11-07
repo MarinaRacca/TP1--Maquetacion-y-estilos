8f716d260df78e1d520725ab3309f883ed9dc241
MarinaRacca <93335011+MarinaRacca@users.noreply.github.com>
2021-11-07 18:31:58 -0300

    Add files via upload

---
+++ Css/display.js
@@ -0,0 +1,31 @@
+function showAlert(){
+    alert ("Iniciá sesión desde la página de inicio para poder comprar")
+}
+function showConfirm(){
+    confirm ("Redirigiendo a una página externa")
+} 
+function Inicio(){
+    Usuario = prompt ("Ingresa tu Usuario");
+    contador = 0;
+    do{
+       Clave=prompt("Ingresa tu clave");
+       if (Clave == "789456123") break;
+       contador++;
+        if(contador ==1) alert ("Te quedan dos intentos");
+        if (contador ==2) alert ("Te queda un intento");
+        } while(contador<3);
+        if (contador ==3){
+            document.write ("cuenta bloqueada");
+        }
+        else{
+            document.write ("Bienvenido a su cuenta");
+        }}
+function Regis(){
+    alert ("Contactate con nostros para que te creemos un Usuario")
+}  
+function ver(n) {
+    document.getElementById("subseccion"+n).style.display="block"
+    }
+function ocultar(n) {
+    document.getElementById("subseccion"+n).style.display="none"
+    }
